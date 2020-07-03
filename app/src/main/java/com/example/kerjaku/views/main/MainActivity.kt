ff8bdc98cf8b1de5caa2c2d50b11ade86403b9f2
Backend Server <backendserver@gmail.com>
2020-07-03 18:39:04 +0700

    Halaman Home

--- app/src/main/java/com/example/kerjaku/views/main/MainActivity.kt
+++ app/src/main/java/com/example/kerjaku/views/main/MainActivity.kt
@@ -2,12 +2,43 @@ package com.example.kerjaku.views.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.kerjaku.R
+import com.example.kerjaku.views.home.HomeFragment
+import com.example.kerjaku.views.taskcomplate.TaskCompleteFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        btmNavMain.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.action_home -> {
+                    openFragment(HomeFragment())
+                    return@setOnNavigationItemReselectedListener true
+                }
+                R.id.action_task_complete -> {
+                    openFragment(TaskCompleteFragment())
+                    return@setOnNavigationItemReselectedListener true
+                }
+            }
+            return@setOnNavigationItemReselectedListener false
+        }
+        btmNavMain.selectedItemId = R.id.action_home
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameMain, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
